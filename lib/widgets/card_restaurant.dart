aa68412df925c466c2902f0d0f3985f3343a4786
fazaimron27 <cincaumyself@gmail.com>
2022-09-23 00:58:43 +0700

    refactor: slice card widget

---
+++ lib/widgets/card_restaurant.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:dicoding_project_restaurant_app/utils/ratings.dart';
+import 'package:dicoding_project_restaurant_app/data/models/restaurant.dart';
+import 'package:dicoding_project_restaurant_app/ui/restaurant_detail.dart';
+
+class CardRestaurant extends StatelessWidget {
+  final Restaurant restaurant;
+  const CardRestaurant({Key? key, required this.restaurant}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, RestaurantDetailPage.routeName,
+            arguments: restaurant.id);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: <Widget>[
+            Hero(
+              tag: restaurant.pictureId,
+              child: Container(
+                width: 130,
+                height: 100,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://restaurant-api.dicoding.dev/images/small/${restaurant.pictureId}'),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    constraints: const BoxConstraints(
+                      maxWidth: 165,
+                    ),
+                    child: Text(
+                      restaurant.name,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(
+                          top: 8,
+                          right: 8,
+                        ),
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.grey,
+                          size: 16,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          padding: const EdgeInsets.only(top: 8),
+                          child: Text(
+                            restaurant.city,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          padding: const EdgeInsets.only(top: 8),
+                          child: ratingStars(restaurant.rating),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
