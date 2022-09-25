09d0dddc2801179839d1d08d357833c1b3bf5a75
fazaimron27 <cincaumyself@gmail.com>
2022-09-26 00:48:20 +0700

    feat: added an internet connection check feature

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,16 @@
+import 'dart:async';
+import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dicoding_project_restaurant_app/common/styles.dart';
 import 'package:dicoding_project_restaurant_app/ui/restaurants.dart';
 import 'package:dicoding_project_restaurant_app/ui/search.dart';
 import 'package:dicoding_project_restaurant_app/data/api/api_service.dart';
 import 'package:dicoding_project_restaurant_app/provider/search_provider.dart';
 import 'package:dicoding_project_restaurant_app/provider/restaurant_provider.dart';
+import 'package:dicoding_project_restaurant_app/utils/custom_error_exception.dart';
 
 class Home extends StatefulWidget {
   static const routeName = '/home';
@@ -17,6 +22,47 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initConnectivity();
+
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  Future<void> initConnectivity() async {
+    late ConnectivityResult result;
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      developer.log('Couldn\'t check connectivity status', error: e);
+      return;
+    }
+
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    setState(() {
+      _connectionStatus = result;
+    });
+  }
+
   int _selectedIndex = 0;
 
   static final List<Widget> _pages = <Widget>[
@@ -40,7 +86,16 @@ class HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: _pages.elementAt(_selectedIndex),
+        child: _connectionStatus == ConnectivityResult.none
+            ? const CustomErrorException(
+                icon: Icon(
+                  Icons.signal_wifi_off,
+                  color: secondaryColor,
+                  size: 100.0,
+                ),
+                message: 'No Internet Connection',
+              )
+            : _pages.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: Container(
         decoration: const BoxDecoration(
