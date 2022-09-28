76b1f5f46ac8b1e1f4ddfa33fec430e69216506a
fazaimron27 <cincaumyself@gmail.com>
2022-09-28 18:08:26 +0700

    refactor: change stateful widget to stateless widget

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -1,25 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:dicoding_project_restaurant_app/common/styles.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   static const routeName = '/splash_screen';
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
+  Widget build(BuildContext context) {
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacementNamed(context, '/home');
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryColor,
       body: Center(
